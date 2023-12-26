0dcd96b54b87cf42ca3398abdd4d7241b8af90f9
Juyoung Lee <configure62@gmail.com>
2023-12-26 22:06:49 +0900

    feat: create signin/signup api

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import { SERVER_PORT } from '../constants/app.constant.js';
+import { apiRouter } from './routers/index.js';
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api', apiRouter);
+
+app.listen(SERVER_PORT, () => {
+  console.log(`${SERVER_PORT}번 포트로 서버가 열렸습니다!`);
+});
---
+++ src/repositories/users.repository.js
@@ -0,0 +1,39 @@
+import bcrypt from 'bcrypt';
+import { PASSWORD_HASH_SALT_ROUNDS } from '../../constants/security.constant.js';
+import { prisma } from '../utils/prisma/index.js';
+
+export class UsersRepository {
+  createOne = async ({ email, password, name }) => {
+    const hashedPassword = bcrypt.hashSync(password, PASSWORD_HASH_SALT_ROUNDS);
+
+    const newUser = await prisma.Users.create({
+      data: { email, password: hashedPassword, name },
+    });
+    delete newUser.password;
+
+    return newUser;
+  };
+
+  readOneById = async (id) => {
+    const user = await prisma.Users.findUnique({ where: { id } });
+
+    return user;
+  };
+
+  readOneByEmail = async (email) => {
+    const user = await prisma.Users.findUnique({ where: { email } });
+
+    return user;
+  };
+
+  promoteOne = async (email) => {
+    const user = await prisma.Users.findUnique({ where: { email } });
+
+    const updateUser = await prisma.Users.update({
+      where: { email },
+      data: {
+        userType: 'MANAGER',
+      },
+    });
+  };
+}
